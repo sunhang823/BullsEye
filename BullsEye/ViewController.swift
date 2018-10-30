645cc582b154210570bae6294d5da4a4d3c9d070
Hang Sun <sunhang0823@gmail.com>
2018-10-30 16:44:52 -0400

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Chris Sun on 10/30/18.
+//  Copyright © 2018 Chris Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
