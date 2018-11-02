129e63a77a0a45976a96527b21370bdcdec048f6
Hang Sun <sunhang0823@gmail.com>
2018-11-02 18:47:55 -0400

    bnmbm

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html"){
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+        
     }
     @IBAction func close(){
         dismiss(animated: true, completion: nil)
