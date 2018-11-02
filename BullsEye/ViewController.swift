4a4e0b502951c13f2f03d1cae400e693f9aba019
Hang Sun <sunhang0823@gmail.com>
2018-11-01 21:40:41 -0400

    zxczxc

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+    }    
+    @IBAction func showAlert(){
+        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        startNewRound()
     }
-
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLables()
+    }
+    func updateLables(){
+        targetLable.text = String(targetValue)
+    }
 
 }
 
